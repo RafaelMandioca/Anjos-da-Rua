294f5b861d0219d5d63cc41accfb62bdb73a1d8d
Yan Lucca Berbert <yanlucca07@gmail.com>
2025-08-05 21:51:49 -0300

    Fix: Browser-Sync consertado
    Consertei a funcionalidade do Browser-sync

--- anjosdaruaproject/settings/development.py
+++ anjosdaruaproject/settings/development.py
@@ -4,5 +4,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# settings/development.py
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
